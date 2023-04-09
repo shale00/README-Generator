119bd663587b630f12724859b56d5481da7caff4
sth <sth0067@gmail.com>
2023-04-08 21:38:30 -0500

    added functions and questions array

--- index.js
+++ index.js
@@ -1,13 +1,125 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your Project Title?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Project Title.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the Description of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project Description.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the Installation instructions?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your installation instructions.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you Use this application?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please describe how to use your app.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Are there any Contributions to your application?',
+            
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What are the Tests of your application?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which License is being used for your application? (Arrow down to the last choice if N/A)',
+            default: 'MIT',
+            choices: ['MIT', 'Apache_2.0', 'GPL_v2', 'CC0', ''],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your email address.');
+                    return false;
+                }
+            }
+        },
+    ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err? console.log(err) : console.log('Success!')
+    );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            const markdown = generateMarkdown(answers);
+            writeToFile('README.md', markdown);
+            console.log('Successfully wrote to README.md.');
+        })
+        .catch((err) => console.log(err));
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,76 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'Apache_2.0') {
+    return `https://opensource.org/licenses/Apache-2.0`
+  }
+  if (license === 'GPL_v2') {
+    return `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`
+  }
+  if (license === 'CC0') {
+    return `http://creativecommons.org/publicdomain/zero/1.0/`
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ''
+  } else {
+    return `## License
+    This app is licensed under the ${license} license.`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Questions about this app?
+  GitHub: https://github.com/${data.github}
+  Email: ${data.email}
 `;
 }
 
